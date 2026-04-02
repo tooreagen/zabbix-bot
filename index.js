9cf3d172116fb9892d59492607c0925e4b956a4b
tooreagen <tooreagen@gmail.com>
2026-04-02 12:14:20 +0300

    add report MAC count

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import cron from "node-cron";
 import { sendWeeklyMacReport } from "./utils/onuMacCountReport.js";
+import { bot } from "./bot.js";
 
 cron.schedule("15 12 * * 4", async () => {
   await sendWeeklyMacReport();
